8db15807d8f97ca37edb73dfaa9678210e0f9b2c
ManishTayde9370 <mptayde9370@example.com>
2025-07-09 11:48:59 +0530

    Analytics

--- backend/src/routes/linksRoutes.js
+++ backend/src/routes/linksRoutes.js
@@ -8,10 +8,17 @@ const router = express.Router();
 router.get('/r/:id',linksController.redirect);
 
 router.use(authMiddleware.protect);
+//This Endpoint needs to be above GET /:id to avoid case where 'analytics' can
+//be  treated as : id.
+router.get('/analytics',authorize('link:read'),linksController.analytics);
 router.post('/',authorize('link:create'),linksController.create);
 router.get('/',authorize('link:read'),linksController.getAll);
 router.get('/:id',authorize('link:read'),linksController.getById);
 router.put('/:id',authorize('link:update'),linksController.update);
 router.delete('/:id',authorize('link:delete'),linksController.delete);
+
+
+
+
 
 module.exports=router;
